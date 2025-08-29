c0dcfffa2caf01b4b3eb2a39f637faee2d3dc242
Mingkun.Zhang <2496808993@qq.com>
2025-08-29 17:57:19 +0800

    [Metax] register deformable_conv kernel & fix 'ModulatedDeformableCol2imCoord' symbol undefined

--- backends/metax_gpu/kernels/cuda_kernels/deformable_conv_kernel_register.cu
+++ backends/metax_gpu/kernels/cuda_kernels/deformable_conv_kernel_register.cu
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,11 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/kernels/gpu/deformable_conv_grad_kernel.cu"  // NOLINT
+#include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/deformable_conv_kernel.h"
+#include "paddle/phi/kernels/impl/deformable_conv_kernel_impl.h"
 
-PD_CUSTOM_KERNEL_REGISTER(deformable_conv_grad,
+PD_CUSTOM_KERNEL_REGISTER(deformable_conv,
                           metax_gpu,
                           ALL_LAYOUT,
-                          phi::DeformableConvGradKernel,
+                          phi::DeformableConvKernel,
                           float,
                           double) {}
