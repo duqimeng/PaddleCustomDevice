06dda181f991db8ed96ee33a60da05139f41142e
Mingkun.Zhang <2496808993@qq.com>
2025-09-01 09:08:54 +0800

    [Metax] fix conflict

--- backends/metax_gpu/kernels/cuda_kernels/deformable_conv_kernel_register.cu
+++ backends/metax_gpu/kernels/cuda_kernels/deformable_conv_kernel_register.cu
@@ -12,10 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/deformable_conv_kernel.h"
-#include "paddle/phi/kernels/impl/deformable_conv_kernel_impl.h"
+#include "paddle/phi/kernels/gpu/deformable_conv_kernel.cu"  // NOLINT
 
 PD_CUSTOM_KERNEL_REGISTER(deformable_conv,
                           metax_gpu,
